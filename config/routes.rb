115cbb463b4822ec570b88d85145e08f0b1bf112
koynonia <koynonia@gmail.com>
2018-09-15 15:46:14 -0300

    040 - Formularios Complexos com Has One (Parte 2-2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'home/index'
+  get '/bemvindo' => 'home#index'
+  root 'home#index'
+  
+  resources :phones
+  resources :addresses
+  resources :contacts #, except: [:edit]
+  resources :kinds
 end
