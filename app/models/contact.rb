754a45983943467305a76796596ff165e7486f7c
koynonia <koynonia@gmail.com>
2018-09-16 10:41:53 -0300

    044 - Associação Has Many

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < ApplicationRecord
   belongs_to :kind, optional: true
   has_one :address, :dependent => :destroy
+  has_many :phones
 
   accepts_nested_attributes_for :address
 end
